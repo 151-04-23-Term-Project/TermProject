6361cf002d76772d5aed110dbbd68c2a1699599b
Elayna (Lana) <62639647+ElaynaSeguin@users.noreply.github.com>
2023-07-15 18:38:26 -0700

    Add files via upload

---
+++ TermProject_1/src/application/SampleController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+	
+	ObservableList<String> questionsList = FXCollections.observableArrayList("Who is your favorite author?","What is your favorite book?","What is your first pet's name?");
+	
+	@FXML private TextField securityQuestionField;
+	
+	@FXML private ComboBox<String> questionOptionsBox;
+	
+	@FXML private void initialize() {
+		questionOptionsBox.setValue("Who is your favorite author?");
+		questionOptionsBox.setItems(questionsList);
+	}
+	
+}
